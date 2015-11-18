20ab4af3bcab37be904228a9c67ef417289bb772
Henrik Svensson <henrik.svensson@capitalage.se>
2015-11-18 09:40:07 +0100

    Adds first version of the db create script.

--- db/setupDbStructure.sql
+++ db/setupDbStructure.sql
@@ -1,11 +1,59 @@
-CREATE SCHEMA `moogerfooger` ;
+DROP SCHEMA IF EXISTS mfd;
 
-USE moogerfooger;
+CREATE SCHEMA mfd ;
 
-create table Presets 
-(
-  preset_id int AUTO_INCREMENT primary key,
-  preset_name nvarchar(100) not null,
-  sort_order int  
-  
-)
+USE mfd;
+
+CREATE TABLE Presets (
+    presetId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    presetNumber INT UNSIGNED NOT NULL,
+    presetName NVARCHAR(100) NOT NULL
+);
+
+CREATE TABLE RangeDimensions (
+    rangeDimensionId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    minPresentationValue INT,
+    maxPresentationValue INT,
+    unit NVARCHAR(100)
+);
+
+CREATE TABLE ListDimensions (
+    listDimensionId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    listName NVARCHAR(100) NOT NULL
+);
+
+CREATE TABLE ListDimensionItems (
+    listDimensionItem INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    listDimensionId INT UNSIGNED NOT NULL,
+    FOREIGN KEY (listDimensionId)
+        REFERENCES ListDimensions (listDimensionId),
+    fromCcValue INT(14) UNSIGNED NOT NULL,
+    throughCcValue INT(14) UNSIGNED NOT NULL,
+    label NVARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Controls (
+    controlId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    listDimensionId INT UNSIGNED,
+    FOREIGN KEY (listDimensionId)
+        REFERENCES ListDimensions (listDimensionId),
+    rangeDimensionId INT UNSIGNED,
+    FOREIGN KEY (rangeDimensionId)
+        REFERENCES RangeDimensions (rangeDimensionId),
+    ccNumberMsb INT(8) UNSIGNED,
+    ccNumberLsb INT(8) UNSIGNED NOT NULL,
+    controlName NVARCHAR(100) NOT NULL,
+    minCcValue INT(14) UNSIGNED NOT NULL,
+    maxCcValue INT(14) UNSIGNED NOT NULL
+);
+
+CREATE TABLE PresetControlValues (
+    presetControlValueId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    presetId INT UNSIGNED,
+    FOREIGN KEY (presetId)
+        REFERENCES Presets (presetId),
+    controlId INT UNSIGNED,
+    FOREIGN KEY (controlId)
+        REFERENCES Controls (controlId),
+    ccValue INT(14) UNSIGNED
+);
