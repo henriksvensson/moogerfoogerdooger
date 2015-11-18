5c5bb455f36bd3cb71e42e09faa12ead853a50d3
Henrik Svensson <henrik.svensson@capitalage.se>
2015-11-18 11:47:02 +0100

    Refactoring and first access to db

---
+++ api.js
@@ -0,0 +1,19 @@
+var mysql      = require('mysql');
+var dbConnection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'mfd',
+  password : 'moogerfoogerdooger',
+  database : 'mfd'
+});
+dbConnection.connect();
+
+var getPresets = function(req, res) {
+	dbConnection.query('SELECT * from mfd.presets', function(err, rows, fields) {
+	  if (!err)
+	    res.end(JSON.stringify(rows));
+	  else
+	    console.log('Error while performing Query.');
+	});
+};
+
+module.exports.getPresets = getPresets;
