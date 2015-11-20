c2152b0a28a40de238254fc52c332a1167d909d0
Henrik Svensson <henrik.svensson@capitalage.se>
2015-11-20 22:00:55 +0100

    Adds edit links on preset rows

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,13 @@
 $(function(){
- $('#testbutton').on('click', function(e){
-     var parameters = { search: $(this).val() };
-       $.get( '/api/getpresets',parameters, function(data) {
-       $('#results').html(data);
-     });
- });
+  $('#testbutton').on('click', function(e){
+    var parameters = { search: $(this).val() };
+      $.get( '/api/getpresets',parameters, function(data) {
+       	$('#results').html(data);
+    });
+ 	});
+
+	$('.presetRow').on('click', function(e){
+	  window.location = '/preset?presetNumber=' + $(this).data("href");
+	});
+
 });
--- routes/routes.js
+++ routes/routes.js
@@ -10,13 +10,13 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/addpreset', function(req, res, next) {
+router.get('/preset', function(req, res, next) {
 	var presetNumber = -1;
 	if(req.query.presetNumber)
 		presetNumber = req.query.presetNumber;
 	db.getControlsNotInPreset(presetNumber, function(err, controlsNotInPreset) {
 		db.getControlsInPreset(presetNumber, function(err, controlsInPreset) {
-      res.render('addpreset', { title: 'Add preset', controlsnotinpreset: controlsNotInPreset, controlsinpreset: controlsInPreset });
+      res.render('preset', { title: 'Preset', controlsnotinpreset: controlsNotInPreset, controlsinpreset: controlsInPreset });
     });
   });
 });
