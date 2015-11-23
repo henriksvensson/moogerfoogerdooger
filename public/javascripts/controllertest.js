a8e53c1a34ff16530bd8bf355cc33e4ece6a9cdc
Henrik Svensson <henrik.svensson@capitalage.se>
2015-11-23 20:04:00 +0100

    Control presentation logic.

--- public/javascripts/controllertest.js
+++ public/javascripts/controllertest.js
@@ -3,35 +3,89 @@ var presets = [
 		presetNumber: 0,
 		presetName: "Preset 1",
 		controls: [{
-			controlId: 0,
-			ccValue: 12,
-			displayValue: "23%"}
+			controlId: 739,
+			ccValue: 1200}, {
+			controlId: 80,
+			ccValue: 64}
 			]
 	},
 	{
 		presetNumber: 1,
 		presetName: "Preset 2",
 		controls: [{
-			controlId: 0,
-			ccValue: 23,
-			displayValue: "Bypassed"}
+			controlId: 80,
+			ccValue: 0}
 			]
 	}
 ];
 
+var controls = [
+	{
+		controlId: 80,
+		ccNumberMSB: null,
+		ccNumberLSB: 80,
+		controlName: "Bypass on/off",
+		minCcValue: 0,
+		maxCcValue: 127,
+		listDimension: [{listLabel: "Bypassed", listValue: 0}, {listLabel: "Active", listValue: 64}]
+	},
+	{
+		controlId: 739,
+		ccNumberMSB: 07,
+		ccNumberLSB: 39,
+		controlName: "Output level",
+		minCcValue: 0,
+		maxCcValue: 16383,
+		rangeDimension: {minPresentationValue: 0, maxPresentationValue: 100, unit: "%"}
+	}
+]
+
 angular.module('myApp', []).
 controller('TestCtrl', function ($scope, $http) {
+
 	$scope.presets = presets;
+	
 	$scope.currentPreset = null;
 	$scope.setCurrentPreset = function(preset){
 		$scope.currentPreset = preset;
 	};
+	
 	$http.get('/api/presets').
       success(function(data, status, headers, config) {
         $scope.presetsDb = data;
     });
 	$http.get('/api/controls').
       success(function(data, status, headers, config) {
-        $scope.allControls = data;
+        $scope.allControls = controls;
     });
+
+ 	$scope.deletePreset = function(presetNumber) {
+ 		for(i = 0; i < presets.length; i++) {
+ 			if(presets[i].presetNumber == presetNumber)
+ 				presets.splice(i, 1);
+ 		}
+ 	}
+
+  $scope.getControlName = function(controlId) {
+    var c = $scope.getControl(controlId);
+		return c.controlName;  	
+  };
+  $scope.getControlPresentationValue = function(controlId, ccValue) {
+    var c = $scope.getControl(controlId);
+    if(!angular.isDefined(c))
+    	return "N/A";
+    if(angular.isDefined(c.listDimension)) 
+    	for(i = 0; i < c.listDimension.length; i++)
+    		if(c.listDimension[i].listValue == ccValue)
+    			return c.listDimension[i].listLabel;
+    if(angular.isDefined(c.rangeDimension))
+    	return ccValue / (c.maxCcValue - c.minCcValue) * c.rangeDimension.maxPresentationValue - c.rangeDimension.minPresentationValue + " " + c.rangeDimension.unit;
+  	return control.ccValue + " [raw]";
+  };
+  $scope.getControl = function(controlId) {
+  	for(c = 0; c < controls.length; c++) 
+  		if(controls[c].controlId == controlId)
+  			return controls[c];
+  	return null;
+  }
 });
