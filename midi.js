94185b0606a0652c6aa4a5c6d2f4b9a4707f1dc8
henrik.svensson@netlight.se <henrik.svensson@netlight.com>
2016-02-09 11:31:04 +0100

    Fixes deprecated sys reference by using shelljs.

--- midi.js
+++ midi.js
@@ -1,21 +1,17 @@
-var sys = require('sys');
-var exec = require('child_process').exec;
+var shell = require('shelljs');
 
-module.exports.dir = function() {
-	exec("dir", function (error, stdout, stderr) {
-	  console.log('stdout: ' + stdout);
-	  console.log('stderr: ' + stderr);
-	  if (error !== null) {
-	    console.log('exec error: ' + error);
-	  }
-	});	
+module.exports.dir = function () {
+    var s = shell.exec("ls", {async: true});
+    s.stdout.on('data', function(data) {
+        console.log(data);
+    });
 };
 
-module.exports.sendPreset = function(preset) {
-	var cmd = "midisnd";
-	for(var c = 0; c < preset.controls.length; c++) {
-		var control = preset.controls[c];
-		var ex = cmd + " " + control.ccValue
-		console.log(ex);
-	}
+module.exports.sendPreset = function (preset) {
+    var cmd = "midisnd";
+    for (var c = 0; c < preset.controls.length; c++) {
+        var control = preset.controls[c];
+        var ex = cmd + " " + control.ccValue
+        console.log(ex);
+    }
 };
